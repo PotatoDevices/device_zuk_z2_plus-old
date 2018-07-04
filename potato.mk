e95b5cdee4833471922d918be33e2ed934e6306a
Anunay Maheshwari <anunaym14@gmail.com>
2018-07-04 15:05:56 +0530

    z2_plus: POSP Bring-up
    Signed-off-by: Anunay Maheshwari <anunaym14@gmail.com>

--- potato.mk
+++ potato.mk
@@ -24,9 +24,9 @@ $(call inherit-product, device/zuk/z2_plus/device.mk)
 # TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_z2_plus
+PRODUCT_NAME := potato_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
